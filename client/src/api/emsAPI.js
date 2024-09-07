74eef000f0b5fa2e73ff8cf0df28443e5e6a0edf
kushal bhargava <kushal.git01@gmail.com>
2024-09-07 15:35:18 +0530

    Service updated

--- client/src/api/emsAPI.js
+++ client/src/api/emsAPI.js
@@ -3,18 +3,8 @@ import Axios from "axios";
 import { API_URL } from "../environment/env_dev";
 
 const emsAPI = Axios.create({
-  baseURL: `${API_URL}/api`,
+  baseURL: `${API_URL}`,
   headers: {},
-});
-
-// shipvistaAPI.CancelToken = Axios.CancelToken;
-
-emsAPI.interceptors.request.use(function (config) {
-  const accessToken = localStorage.getItem(accessTokenSessionKey);
-
-  config.headers.Authorization = `Bearer ${accessToken}`;
-
-  return config;
 });
 
 export default emsAPI;
